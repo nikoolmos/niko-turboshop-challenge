3c5862224484632bc37619d7165d879cc854eb5c
Nicolás Olmos <nicolasolmoscorreo@gmail.com>
2026-05-13 23:07:40 -0300

    add base file for auto parts plus part data normalizer

---
+++ niko-turbo-challenge-be/src/auto-parts-plus-part-data-normalizer/auto-parts-plus-part-data-normalizer.spec.ts
@@ -0,0 +1,7 @@
+import { AutoPartsPlusPartDataNormalizer } from './auto-parts-plus-part-data-normalizer';
+
+describe('AutoPartsPlusPartDataNormalizer', () => {
+  it('should be defined', () => {
+    expect(new AutoPartsPlusPartDataNormalizer()).toBeDefined();
+  });
+});
