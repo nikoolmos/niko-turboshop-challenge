0b8eeda6dbf1a61eb862ebdc84ee760e954c71b8
Nicolás Olmos <nicolasolmoscorreo@gmail.com>
2026-05-13 23:06:18 -0300

    add logic to part request handler

--- niko-turbo-challenge-be/src/part-request-handler/part-request-handler.ts
+++ niko-turbo-challenge-be/src/part-request-handler/part-request-handler.ts
@@ -1,1 +1,22 @@
-export class PartRequestHandler {}
+import { PartDataSourceFactory } from "src/part-data-source-factory/part-data-source-factory";
+import { PartDataSource } from "src/part-data-source/part-data-source";
+
+export class PartRequestHandler {
+    private dataSources: Array<PartDataSource>;
+    
+    constructor() {
+        this.dataSources = PartDataSourceFactory.createPartDataSources();    
+    }
+
+    public getAllCatalogs(page: string, limit: string) {
+        const myPromisesArray: Array<any> = [];
+
+        for(const datasource of this.dataSources) {
+            myPromisesArray.push(datasource.getCatalog(page, limit));
+        }
+
+        const finalPromise = Promise.allSettled(myPromisesArray);
+
+        return finalPromise;
+    }
+}
