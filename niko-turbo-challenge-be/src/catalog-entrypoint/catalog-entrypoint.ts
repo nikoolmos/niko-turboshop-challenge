e6036fa74a70291313f7ba5ea1be7c4fc6211be1
Nicolás Olmos <nicolasolmoscorreo@gmail.com>
2026-05-13 23:05:40 -0300

    add content to entrypoint file

--- niko-turbo-challenge-be/src/catalog-entrypoint/catalog-entrypoint.ts
+++ niko-turbo-challenge-be/src/catalog-entrypoint/catalog-entrypoint.ts
@@ -1,1 +1,15 @@
-export class CatalogEntrypoint {}
+import { PartDataNormalizer } from "src/part-data-normalizer/part-data-normalizer";
+import { PartRequestHandler } from "src/part-request-handler/part-request-handler";
+
+export class CatalogEntrypoint {
+
+    public static getPartsCatalog(page: string, limit: string) {
+        const myPartRequestHandler = new PartRequestHandler();
+        const partsData = myPartRequestHandler.getAllCatalogs(page, limit);
+
+        const myPartDataNormalizer = new PartDataNormalizer();
+        myPartDataNormalizer.normalizeCatalogData(partsData);
+        
+        
+    }
+}
