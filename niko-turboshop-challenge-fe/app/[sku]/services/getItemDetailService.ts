426274242d4b2cb58138bca772f6d067363bd59b
Nicolás Olmos <nicolasolmoscorreo@gmail.com>
2026-05-18 22:53:40 -0300

    remote unnecesary code

--- niko-turboshop-challenge-fe/app/[sku]/services/getItemDetailService.ts
+++ niko-turboshop-challenge-fe/app/[sku]/services/getItemDetailService.ts
@@ -1,4 +1,3 @@
-import { GetItemDetailServiceResponse } from "../interfaces/getItemDetailServiceResponse";
 import { PartDetail } from "../interfaces/PartDetail";
 
 export class GetItemDetailService {
@@ -6,14 +5,8 @@ export class GetItemDetailService {
         const url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/item-detail`);
         url.searchParams.set('id', id)
         url.searchParams.set('provider', provider);
-        console.log('REQ', url.toString())
         const response = await fetch(url);
         const data = await response.json();
-
         return data;
-        
-        
-        
-        //as GetItemDetailServiceResponse;
     }
 }
